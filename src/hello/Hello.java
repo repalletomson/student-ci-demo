5b5ca7f0528c12952d5cbf2b61d19128aa440d1d
repalletomson <169348181+repalletomson@users.noreply.github.com>
2025-10-06 15:23:55 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,10 +3,8 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Pipeline !");
-        System.out.println("Hello from Poll SCM trigger!");
-        System.out.println("Hello from Poll SCM trigger!");
-        System.out.println("Checking the trigger github");
-        System.out.println("Hello from Poll SCM trigger!");
-        System.out.println("Hello from Poll SCM trigger!");
+
+         System.out.println("Checking the  agents !");
+
     }
 }
